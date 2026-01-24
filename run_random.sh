2584f406a625d3f781360049aa37957912b5094f
Frederik Kirk <kaae-2@hotmail.com>
2026-01-24 18:50:10 +0100

    metrics

--- run_random.sh
+++ run_random.sh
@@ -1,13 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
 
+script_dir="$(cd -- "$(dirname -- "$0")" && pwd)"
 
-data_dir="/Users/srz223/Documents/courses/Benchmarking/repos/ob-pipeline-cytof/out/data/data_import/dataset_name-FR-FCM-Z2KP_virus_final_seed-42/preprocessing/data_preprocessing/num-1_test-sample-limit-5"
-script_dir="/Users/srz223/Documents/courses/Benchmarking/repos/ob-pipeline-LDA"
-
-Rscript "${script_dir}/ob-pipeline-random.R"\
-  --name "lda" \
-  --output_dir "${script_dir}/out_test" \
-  --data.train_matrix "${data_dir}/data_import.train.matrix.tar.gz" \
-  --data.train_labels "${data_dir}/data_import.train.labels.tar.gz" \
-  --data.test_matrix "${data_dir}/data_import.test.matrices.tar.gz" 
-  
-  
+Rscript "${script_dir}/ob-pipeline-random.R" \
+  --name "random" \
+  --output_dir "${script_dir}/out/data/analysis/default/random" \
+  --data.train_matrix "${script_dir}/out/data/data_preprocessing/default/data_import.train.matrix.tar.gz" \
+  --data.train_labels "${script_dir}/out/data/data_preprocessing/default/data_import.train.labels.tar.gz" \
+  --data.test_matrix "${script_dir}/out/data/data_preprocessing/default/data_import.test.matrices.tar.gz"
